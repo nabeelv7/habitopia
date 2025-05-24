d91e5a3aeb9919941608520bfa17949bfdecbffd
nabeelv7 <nabeelsadiq.dev@gmail.com>
2025-05-24 20:45:29 +0500

    dock

--- src/lib/components/Dock.svelte
+++ src/lib/components/Dock.svelte
@@ -6,18 +6,27 @@
            md:top-0 md:left-0 md:bottom-0 md:w-auto md:h-full md:flex-col
            md:items-center md:border-r md:border-r-base-300 border-t-base-300 md:border-t-0"
 >
-  <button class="p-4 flex flex-col items-center md:p-6 cursor-pointer">
+  <a
+    href="/dashboard/habits"
+    class="p-4 flex flex-col items-center md:p-6 cursor-pointer"
+  >
     <h1 class="text-3xl">🧘</h1>
-    <a href="/dashboard/habits" class="text-xs mt-1 md:mt-2">Habits</a>
-  </button>
+    <span class="text-xs mt-1 md:mt-2">Habits</span>
+  </a>
 
-  <button class="p-4 flex flex-col items-center md:p-6 cursor-pointer">
+  <a
+    href="/dashboard/zoo"
+    class="p-4 flex flex-col items-center md:p-6 cursor-pointer"
+  >
     <h1 class="text-3xl">🐵</h1>
-    <a href="/dashboard/zoo" class="text-xs mt-1 md:mt-2">Zoo</a>
-  </button>
+    <span class="text-xs mt-1 md:mt-2">Zoo</span>
+  </a>
 
-  <button class="p-4 flex flex-col items-center md:p-6 cursor-pointer">
+  <a
+    href="/dashboard/streaks"
+    class="p-4 flex flex-col items-center md:p-6 cursor-pointer"
+  >
     <h1 class="text-3xl">📈</h1>
-    <a href="/dashboard/streaks" class="text-xs mt-1 md:mt-2">Streaks</a>
-  </button>
+    <span class="text-xs mt-1 md:mt-2">Streaks</span>
+  </a>
 </div>
