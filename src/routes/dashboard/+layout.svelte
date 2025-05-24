4c2cc7327c6e23b0152559d335101a7ca8e69f8c
nabeelv7 <nabeelsadiq.dev@gmail.com>
2025-05-24 21:14:16 +0500

    added proper margins to respect docks

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -1,14 +1,19 @@
 <script>
-  import Sidebar from "$lib/components/Dock.svelte";
+  import Dock from "$lib/components/Dock.svelte";
   let { data, children } = $props();
-  console.log("hello");
 </script>
 
-<div
-  class="tooltip tooltip-left fixed top-10 right-10 text-3xl font-extrabold font-mono animate-bounce"
-  data-tip="These are gems, which are earned through completing habits regularly and can be used to buy new pets in the pet store."
->
-  <h1>💎 11</h1>
+<!-- Fixed sidebar -->
+<Dock />
+
+<!-- Main content -->
+<div class="md:ml-33 ml-0 max-md:mb-23">
+  <div
+    class="tooltip tooltip-left fixed top-10 right-10 text-3xl font-extrabold font-mono"
+    data-tip="These are gems, which are earned through completing habits regularly and can be used to buy new pets in the pet store."
+  >
+    <h1 class="md:btn md:btn-xl">💎 11</h1>
+  </div>
+
+  {@render children()}
 </div>
-<Sidebar />
-{@render children()}
